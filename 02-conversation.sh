0d9bbb6ca2c330797805cbca0b6a34cf7cf581e7
Divya Arekatla <divyaare97@gmail.com>
2025-09-19 11:43:16 -0400

    Conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi suresh, How are you?"
+echo "Suresh:: Hello ramesh, i am fine, how are you?"
+echo "Ramesh:: what are you learning recently?"
+echo "Suresh:: I am learning shell scripting Ramesh"
+
