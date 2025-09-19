5ced1730d6b78ab3d82edf46d2993450d4555779
Divya Arekatla <divyaare97@gmail.com>
2025-09-19 11:56:03 -0400

    Variables

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=John
+PERSON2=Raheem
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, i am fine, how are you?"
+echo "$PERSON1:: what are you learning recently?"
+echo "$PERSON2:: I am learning shell scripting $PERSON1"
+
