a39af41648dc15d16cafcfa9c3ad4ff01ef3f596
Divya Arekatla <divyaare97@gmail.com>
2025-09-19 12:11:33 -0400

    variables 2

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Welcome to alaska $PERSON2"
+echo "$PERSON2:: Thank you $PERSON1, Finally not on zoom webinar"
+echo "$PERSON1:: so, do we have a deal"
+echo "$PERSON2:: yes, if you make ukraine joins russia"
