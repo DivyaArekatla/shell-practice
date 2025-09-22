8562c46c5193ea21a0a2cf79fecfdcdce4beacb1
Divya Arekatla <divyaare97@gmail.com>
2025-09-22 13:12:36 -0400

    conditions

---
+++ 13-installation.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root prevelege"
+    exit 1
+
+fi
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]; then
+        echo "ERROR:: Install $2 is failure"
+        exit 1
+    else
+        echo "Installing $2 is success"
+    fi
+
+}
+
+dnf install mysql -y
+VALIDATE $? "MySQL"
+
+dnf install nginx -y
+VALIDATE $? "Nginx"
+
+dnf install python3 -y
+VALIDATE $? "Python3"
+
+
+
