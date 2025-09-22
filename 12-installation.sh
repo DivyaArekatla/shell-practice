1787003fc82fd0bb0748b349816cca63bdb5d65c
Divya Arekatla <divyaare97@gmail.com>
2025-09-22 12:53:51 -0400

    conditions

---
+++ 12-installation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root prevelege"
+    exit 1
+
+fi
+
+dnf install mysql -y
+
+if[ $? -ne 0 ]; then
+    echo "ERROR:: Install mysql is failure"
+    exit 1
+else
+    echo "Installing mysql is success"
+fi
